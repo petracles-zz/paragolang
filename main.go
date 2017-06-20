440953905a176ed42a7581d6cf9e6738296edf43
Jack Kelly <petracles@gmail.com>
2017-06-20 18:39:01 -0400

    Added initial HTTPS setup

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/kabukky/httpscerts"
 )
 
 
 func main() {
+	// Instantiate the mux router
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe("", router))
+
+	/* Build the HTTPS certs. This is currently only a self-cert for development, and
+	a real HTTPS cert from a CA will be needed */
+    err := httpscerts.Check("cert.pem", "key.pem")
+    // If they are not available, generate new ones.
+    if err != nil {
+        err = httpscerts.Generate("cert.pem", "key.pem", ":8080")
+        if err != nil {
+            log.Fatal("Error: Couldn't create https certs.")
+        }
+    }
+
+	log.Fatal(http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", router))
 }
